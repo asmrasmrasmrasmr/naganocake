be8b22ced8bf41e6770caccc5698cfaa7cb0dd00
EC2 Default User <ec2-user@ip-172-31-41-197.ec2.internal>
2022-03-18 09:02:38 +0000

    ジャンル機能のモデル、コントローラー、ビュー作成、編集中

---
+++ app/controllers/admin/genres_controller.rb
@@ -0,0 +1,30 @@
+class Admin::GenresController < ApplicationController
+
+  def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to admin_genres_path
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    genre = Genre.find
+    genre.update(genre_params)
+    redirect_to edit_admin_genre_path
+    
+
+  end
+
+  private
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+end
