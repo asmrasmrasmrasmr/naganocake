f4bb607ce0dcb209f4a7ae096a0fb4f578970f25
EC2 Default User <ec2-user@ip-172-31-22-147.ec2.internal>
2022-03-19 10:51:16 +0000

    顧客側の配送先登録、一覧、編集を追加しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2022_03_20_072046) do
+=======
+ActiveRecord::Schema.define(version: 2022_03_19_073539) do
+>>>>>>> 2998dcb (顧客側の配送先登録、一覧、編集を追加しました)
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -44,7 +48,7 @@ ActiveRecord::Schema.define(version: 2022_03_20_072046) do
     t.integer "customer_id"
     t.string "name"
     t.string "postal_code"
-    t.string "addresses"
+
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "address"
