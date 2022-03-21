8663b1bdfc9e19c4aa0729f37fe1d458d85a3256
EC2 Default User <ec2-user@ip-172-31-40-191.ec2.internal>
2022-03-21 05:20:23 +0000

    全員集合！

--- db/schema.rb
+++ db/schema.rb
@@ -115,16 +115,6 @@ ActiveRecord::Schema.define(version: 2022_03_20_090219) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "order_items", force: :cascade do |t|
-    t.integer "order_id"
-    t.integer "item_id"
-    t.integer "amount"
-    t.integer "status"
-    t.integer "total"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "orders", force: :cascade do |t|
     t.integer "customer_id"
     t.integer "shipping_fee"
@@ -136,18 +126,6 @@ ActiveRecord::Schema.define(version: 2022_03_20_090219) do
     t.integer "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
